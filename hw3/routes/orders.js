b512587deb3df31abdce2800a8aeeb0409c1c9bd
Sean Yang <yangs28@up.edu>
2026-02-09 15:55:17 -0800

    Reworked the orders.js file to display the json data via res.json instead. Added a rudimentary data table

--- hw3/routes/orders.js
+++ hw3/routes/orders.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET orders page. */
 router.get('/', function(req, res, next) {
-    res.render('orders', { title: 'Orders' });
+res.json({
+    title: 'Orders',
+    data: [
+    {"topping":"cherry", "quantity":2},
+    {"topping":"plain", "quantity":6},
+    {"topping":"chocolate", "quantity":3}
+]
+});
 });
 
 module.exports = router;
