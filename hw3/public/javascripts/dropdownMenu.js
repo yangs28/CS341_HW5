5880aca6e14b39e0d6221ba83138ddeeaf5178d3
Sean Yang <yangs28@up.edu>
2026-02-10 13:06:54 -0800

    Added prototype post for dropdownMenu. Server side handling is not yet implemented

--- hw3/public/javascripts/dropdownMenu.js
+++ hw3/public/javascripts/dropdownMenu.js
@@ -9,6 +9,13 @@
       //When a month is selected from the drop-down, change the month accordingly 
     $(".dropdown-content a").click(function(){
         var selectedMonth = $(this).text();
+        const button = document.getElementById("dropbtn");
         $("#dropbtn").text(selectedMonth);
+        alert("You have selected " + selectedMonth);
+
+        $.post("/orders", {selectedMonth}, function(orders) {
+            console.log(orders);
+        });
+
     });
-    });
+});
